af7747f349c12408cc36e22ffcd92ac37ae09072
GriffinColomer <griffincolomer@gmail.com>
2024-08-03 03:42:59 -0700

    small changes

--- src/build.py
+++ src/build.py
@@ -15,14 +15,17 @@ def build(text) -> str:
 def main(argv: list[str]) -> None:
     if len(argv) == 1:
         raise ValueError("need to specify file")
-    path = os.getcwd() + "\\" + argv[1]
+    path = os.getcwd() + "/" + argv[1]
+
     file = open(path, "r")
     contents = file.read()
     file.close()
+
     results = build(contents)
-    file = open(os.getcwd()+"\\"+argv[2], "w")
+    file = open(os.getcwd()+"/"+argv[2], "w")
     file.write(results)
     file.close
+
     print('file is successfully compiled')
     
 if __name__ == '__main__':
